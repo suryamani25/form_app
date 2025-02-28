467bd57c00ab74c7389b9a737ea1bf31ae2ed8c0
Surya Mani Verma <93043048+suryamani25@users.noreply.github.com>
2025-02-28 23:47:57 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 const db = mysql.createConnection({
   host: 'localhost', 
   user: 'root', 
-  password: '7348457098', 
+  password: 'password', 
   database: 'form_app', 
 });
 
